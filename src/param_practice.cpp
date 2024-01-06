e86097967a0fe15c42c96f6cdd06aa359c7d6d76
Wang-hsiu-chen <wang.tw.ted@gmail.com>
2024-01-07 00:25:41 +0800

    topic, message, param, launch

---
+++ src/param_practice.cpp
@@ -0,0 +1,18 @@
+#include <ros/ros.h>
+
+int main(int argc, char **argv){
+
+    ros::init(argc,argv,"param_practice");
+    ros::NodeHandle nh;
+    double freq = 2;
+
+    while(ros::ok()){
+        
+        nh.getParam("/print_freq",freq);
+        ros::Rate rate(freq);
+        ROS_INFO("%f", freq);
+        rate.sleep();
+
+    }
+    return 0;
+}
